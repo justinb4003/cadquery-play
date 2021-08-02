84fe55dc65d2b09dd9d6556e97d67b926eeea80a
Justin Buist <justin.buist@gmail.com>
2021-08-01 23:01:34 -0400

    Completes the wedge shape

--- hathook.py
+++ hathook.py
@@ -3,25 +3,30 @@ import cadquery as cq
 hook_width = 12.0
 hook_thickness = 4.0
 clamp_depth = 25.0
-shelf_height = 20.0
-corner_radius = 3.0
+corner_radius = 2.0
+
+shelf_height = 8.0
 
 hook = (
     cq.Workplane("XY")
-      .rect(hook_width+hook_thickness, clamp_depth)
+      .rect(hook_width, clamp_depth+hook_thickness)
       .extrude(shelf_height + 2*hook_thickness)
 )
 
+hook = hook.edges("|Y").fillet(corner_radius)
+hook = hook.edges(">Y").fillet(corner_radius)
+hook = hook.edges("<Y").fillet(corner_radius)
+
 shelf = (
-    hook.faces('>Z')
-        .workplane(-hook_thickness, True)
-        .rect(clamp_depth, hook_width)
-        .extrude(shelf_height, False)
+    hook.faces('<Z')
+        .workplane(hook_thickness, invert=True)
+        .center(0, -hook_thickness)
+        .rect(hook_width, clamp_depth)
+        .extrude(shelf_height, combine=False)
 )
 
 hook = hook.cut(shelf)
 
-# hook = hook.edges("|Y").fillet(corner_radius)
 # hook = hook.edges("|X").fillet(corner_radius)
 
 cq.exporters.export(hook, 'hathook.stl')
