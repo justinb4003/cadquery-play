f4226ab41fd49e0dcc1ab6a5164c4707954f5372
Justin Buist <justin.buist@gmail.com>
2021-08-06 08:36:23 -0400

    Made x-cord calculations less jank

--- marker_rack.py
+++ marker_rack.py
@@ -1,13 +1,17 @@
 import cadquery as cq
+from math import floor
 
-marker_count = 2
+total_markers = 8
+marker_rows = 3
+mmpr = 3  # max markers per row, gets used alot, so it's a short name
 
 md_min = 17.0
 md_max = 23.0
-mspacing = 1.2
+md_pad = 4.5
+md_full = md_max+md_pad
 block_height = 80.0
-block_width = (md_max*(marker_count+1)*mspacing)
-block_depth = md_max*1.4
+block_width = md_full*(mmpr+0.5)
+block_depth = (md_max*1.4)*marker_rows
 
 block = (
     cq.Workplane("XY")
@@ -15,37 +19,74 @@ block = (
       .extrude(block_height)
 )
 
-slots = []
-for i in range(marker_count):
-    testing = False
-    peg = (
-        block.faces(">Z")
-            .workplane(invert=not testing)
-            .center(-block_width/2, 0)  # slam pointer to far side of block 
-            .center((i+1)*md_max*mspacing, 0)
-            .circle(md_max/2)
-            .extrude(block_height*0.9, combine=False)
-    )
-    
-    taper = (
-        block.faces(">Z")
-            .workplane(offset=block_height*.9, invert=not testing)
-            .center(-block_width/2, 0)  # slam pointer to far side of block 
-            .center((i+1)*md_max*mspacing, 0)
-            .circle(md_max/2)
-            .workplane(offset=block_height*.1)
-            .circle(md_min/2)
-            .loft(combine=False)
-    )
-    slot = peg.union(taper)
-    print('chop chop')
-    slots.append(slot)
-for s in slots:
-    if testing:
-        block = block.union(s)
-    else:
-        block = block.cut(s)
 block = block.edges("|Z").fillet(12.0)
+
+base_padding = 4.0
+base_height = 12.0
+base_pocket = 8.0
+base = (
+    cq.Workplane("XY")
+      .workplane(offset=base_pocket-base_height)
+      .rect(block_width+base_padding*2, block_depth+base_padding*2)
+      .extrude(base_height)
+)
+base = base.edges("|Z").fillet(12.0)
+base = base.edges(">Z").fillet(4.0)
+
+base = base.cut(block)
+
+
+(slots, x, y) = ([], 0, 0)
+for r in range(1, marker_rows+1):
+    marker_count = mmpr-1 if r % 2 == 0 else mmpr
+    
+    for i in range(marker_count):
+        testing = False
+        
+        xoffset = md_full/4
+        if marker_count == mmpr:
+            xoffset *= -1
+        newx = (i+1)*(md_full) + xoffset
+
+        if r == 1:
+            newy = -md_full
+        elif r==2:
+            newy = 0
+        elif r==3:
+            newy = md_full
+        else:
+            print('what?!')
+        deltax = newx - x
+        x = newx 
+        y = newy
+        print(f'{x}x{y}, {deltax}')
+        peg = (
+            block.faces(">Z")
+                .workplane(invert=not testing)
+                .center(-block_width/2, 0)  # slam pointer to far side of block 
+                .center(x, y)
+                .circle(md_max/2)
+                .extrude(block_height*0.9, combine=False)
+        )
+        
+        taper = (
+            block.faces(">Z")
+                .workplane(offset=block_height*.9, invert=not testing)
+                .center(-block_width/2, 0)  # slam pointer to far side of block 
+                .center(x, y)
+                .circle(md_max/2)
+                .workplane(offset=block_height*.1)
+                .circle(md_min/2)
+                .loft(combine=False)
+        )
+        slot = peg.union(taper)
+        slots.append(slot)
+    for s in slots:
+        if testing:
+            block = block.union(s)
+        else:
+            block = block.cut(s)
 block = block.edges(">Z").fillet(2.0)
 
 cq.exporters.export(block, 'marker_rack.stl')
+cq.exporters.export(base, 'marker_base.stl')
