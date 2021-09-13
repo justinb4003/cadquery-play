8b3e0fa1af9445ad7e04d92a53f549791edc20b3
Justin Buist <justin.buist@gmail.com>
2021-09-13 08:08:54 -0400

    Janky wheel with 1.875" hole spacing

--- toast_wheel.py
+++ toast_wheel.py
@@ -2,7 +2,7 @@ import cadquery as cq
 
 inch = 25.4
 
-wheel_width = 0.25 * inch
+wheel_width = 0.75 * inch
 wheel_diameter = 6 * inch
 
 pattern_length = 1.875 * inch / 2
@@ -19,13 +19,22 @@ wheel = (
         .circle(1.25*inch/2)
         .cutThruAll()
 )
+
 wheel = (
     wheel.faces(">Z")
         .workplane()
         .polygon(6, 1.875*inch, forConstruction=True)
         .vertices()
-        .circle((3/16)*inch/2)
-        .cutThruAll()
+        .cboreHole((3/16)*inch, (3/8)*inch, (1/4)*inch)
+)
+
+wheel = (
+    wheel.faces(">Z")
+        .workplane(invert=True)
+        .polygon(30, wheel_diameter, forConstruction=True)
+        .vertices()
+        .polygon(6, 8)
+        .extrude(wheel_width)
 )
 
 cq.exporters.export(wheel, 'output/toast_wheel.stl')
