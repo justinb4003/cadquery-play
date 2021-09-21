1810c349c76ae97a449750df6b4c8b2b6086a662
Justin Buist <justin.buist@gmail.com>
2021-09-20 21:52:35 -0400

    Mount for 37mm diameter motor (rough idea)

--- toast_motor.py
+++ toast_motor.py
@@ -7,13 +7,17 @@ motor_od = 1.5 * inch
 motor_depth = 2.5 * inch
 mount_spacing = 1.25 * inch
 mount_screw_od = (10/64) * inch
-nub_width = 0.5 * inch
+motor_tiedown_od = (1/8) * inch  # Yeah I know
+nub_od = 0.5 * inch
 nub_depth = 0.25 * inch
+wall_thickness = 0.25 * inch
+wall_height = motor_od+inch
+wall_length = motor_od + 2*inch
 
 mount = (
     cq.Workplane("YZ")
-      .rect(motor_od+inch, motor_od + 2*inch)
-      .extrude(0.25 * inch)
+      .rect(wall_height, wall_length)
+      .extrude(wall_thickness)
 )
 
 mount = (
@@ -21,12 +25,6 @@ mount = (
        .polygon(6, mount_spacing, forConstruction=True)
        .vertices()
        .circle(mount_screw_od / 2)
-       .cutThruAll()
-)
-
-mount = (
-  mount.faces(">X")
-       .circle(nub_width / 2)
        .cutThruAll()
 )
 
@@ -38,15 +36,34 @@ motor_cut = (
        .extrude(motor_depth, combine=False)
 )
 
+nub_cut = (
+  mount.faces(">X")
+       .circle(nub_od / 2)
+       .extrude(-wall_thickness, combine=False)
+)
+
 base = (
-    cq.Workplane("XZ")
-      .workplane(offset=0, invert=True)
-      .center((motor_od+inch)/2, 0)
-      .rect(motor_depth + inch, base_width)
-      .extrude(1 * inch)
+  mount.faces(">X")
+       .workplane()
+       .center(wall_height/4, 0)
+       .rect(wall_height/2, wall_length)
+       .extrude(motor_depth, combine=False)
+)
+
+motor_strap_holes = (
+  base.faces("<Y")
+      .workplane()
+      .center(inch, 0)
+      #rect(uppy-down, lefty-right)
+      .rect(1.25*inch, motor_od+inch, forConstruction=True)
+      .vertices()
+      .circle(motor_tiedown_od / 2)
+      .extrude(-0.5*inch, combine=False)
 )
 
 mount = mount.union(base)
+mount = mount.cut(motor_strap_holes)
+mount = mount.cut(nub_cut)
 mount = mount.cut(motor_cut)
 
 cq.exporters.export(mount, 'output/toast_motor.stl')
