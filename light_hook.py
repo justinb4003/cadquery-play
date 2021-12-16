67e49da0e757b6592fa358c2ecad325c00afa602
Justin Buist <justin.buist@gmail.com>
2021-12-15 22:06:01 -0500

    Clean enough for me right now

--- light_hook.py
+++ light_hook.py
@@ -3,32 +3,41 @@ import cadquery as cq
 top_hanger_id = 12.5
 bottom_hanger_id = 30.0
 thickness = 4.0
-width = 14
-drop_height = 15
+width = 8
+drop_height = 25 
 
-def make_half_circle(r):
+def make_half_circle(wp, r):
   hc = (
-    cq.Workplane("XY")
-      .lineTo(-r, 0)
+    wp.lineTo(-r, 0)
       .threePointArc((0, r), (r, 0))
       .close()
   )
   return hc
 
 
-top_cut = make_half_circle(top_hanger_id/2).extrude(width)
-top_hanger = make_half_circle(top_hanger_id/2+(thickness)).extrude(width)
+top_cut = make_half_circle(cq.Workplane("XY"), top_hanger_id/2).extrude(width)
+top_hanger = make_half_circle(cq.Workplane("XY"), top_hanger_id/2+(thickness)).extrude(width)
 hook = top_hanger.cut(top_cut)
 hook = (
   hook.faces("<Y")
-  .workplane(centerOption="CenterOfMass")
-  .circle(1)
-  .extrude(10)
+  .workplane(centerOption="CenterOfBoundBox")
+  .center(top_hanger_id/2+thickness/2, 0)
+  .rect(thickness, width)
+  .extrude(5)
+  .center(-(top_hanger_id+thickness), 0)
+  .rect(thickness, width)
+  .extrude(drop_height)
 )
-"""
+
+d = 30
+bottom_hanger = make_half_circle(cq.Workplane("XY").center(4.75, -drop_height), 
+                                 -(d/2)).extrude(width)
+
+bottom_cut = make_half_circle(cq.Workplane("XY").center(4.75, -drop_height), 
+                              -(d/2-thickness)).extrude(width)
+
+bottom_hanger = bottom_hanger.cut(bottom_cut)
+hook = hook.union(bottom_hanger)
 hook = hook.edges("|Z").fillet(1.5)
-hook = hook.cut(hanger_cut)
-hook = hook.faces(">X").edges(">Y").fillet(3)
-"""
 
 cq.exporters.export(hook, 'output/light_hook.stl')
